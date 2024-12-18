006b21c41d486e7ecc20e6e3c2d0e1acef23a46c
Edward Tanguay (windows) <edward@tanguay.info>
2024-12-18 22:23:48 +0100

    figured out getting state

--- src/store/models/flashcardModel.ts
+++ src/store/models/flashcardModel.ts
@@ -34,11 +34,11 @@ export interface FlashcardModel {
 	addWrongAnswer: Action<this, string>;
 	resetTestingFlashcard: Action<this>;
 	setFilteredFlaschards: Action<this, string>;
-	addAmountToFlashcardFilterItems: Action<this>;
 
 	// thunks
 	setNextTestingFlashcard: Thunk<this, void, void, StoreModel>;
 	loadFlashcards: Thunk<this, void, void, StoreModel>;
+	addAmountToFlashcardFilterItems: Thunk<this, void, void, StoreModel>;
 }
 
 export const flashcardModel: FlashcardModel = {
@@ -114,6 +114,7 @@ export const flashcardModel: FlashcardModel = {
 				state.filteredFlashcards = state.flashcards
 					.sort((a, b) => (a.whenCreated > b.whenCreated ? -1 : 1))
 					.slice(0, 10);
+				console.log(11115, state.filteredFlashcards.length);
 				break;
 			case "latest20":
 				state.filteredFlashcards = state.flashcards
@@ -126,11 +127,6 @@ export const flashcardModel: FlashcardModel = {
 					.slice(0, 50);
 				break;
 		}
-	}),
-	addAmountToFlashcardFilterItems: action((state) => {
-		state.flashcardFilterItems.forEach((ffi) => {
-			ffi.amount = 999;
-		});
 	}),
 
 	// thunks
@@ -157,5 +153,11 @@ export const flashcardModel: FlashcardModel = {
 		state.flashcards = dataModel.getFlashcards();
 		state.filteredFlashcards = structuredClone(state.flashcards);
 		actions.addAmountToFlashcardFilterItems();
+	}),
+	addAmountToFlashcardFilterItems: thunk((actions, _, { getState }) => {
+		getState().flashcardFilterItems.forEach((ffi) => {
+			actions.setFilteredFlaschards(ffi.idCode);
+			ffi.amount = getState().filteredFlashcards.length;
+		});
 	}),
 };
